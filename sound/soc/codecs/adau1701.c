50ccc87154d3cb2c6974349fdcadaec8afda346c
Daniel Mack <zonque@gmail.com>
2013-10-31 14:04:34 +0100

    ASoC: adau1701: ADAU1701_DSPCTRL non-volatile
    Signed-off-by: Pascal Huerst <pascal.huerst@gmail.com>

--- sound/soc/codecs/adau1701.c
+++ sound/soc/codecs/adau1701.c
@@ -174,7 +174,6 @@ static bool adau1701_volatile_reg(struct device *dev, unsigned int reg)
 {
 	switch (reg) {
 	case ADAU1701_DACSET:
-	case ADAU1701_DSPCTRL:
 		return true;
 	default:
 		return false;
