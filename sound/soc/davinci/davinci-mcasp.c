848d863485f0c242944e90a9e2354e1986bfb8cf
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-07-16 15:12:04 +0300

    ASoC: davinci-mcasp: Use dmaengine based platform driver for AM335x/447x
    Use the edma-pcm with AM335x and AM437x SoCs.
    Keep using the davinci-pcm for daVinci devices, they can be switched to use
    the dmaengine based driver later when they are verified to work correctly.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/davinci/davinci-mcasp.c
+++ sound/soc/davinci/davinci-mcasp.c
@@ -36,6 +36,7 @@
 #include <sound/omap-pcm.h>
 
 #include "davinci-pcm.h"
+#include "edma-pcm.h"
 #include "davinci-mcasp.h"
 
 #define MCASP_MAX_AFIFO_DEPTH	64
@@ -778,7 +779,7 @@ static int davinci_mcasp_dai_probe(struct snd_soc_dai *dai)
 {
 	struct davinci_mcasp *mcasp = snd_soc_dai_get_drvdata(dai);
 
-	if (mcasp->version == MCASP_VERSION_4) {
+	if (mcasp->version >= MCASP_VERSION_3) {
 		/* Using dmaengine PCM */
 		dai->playback_dma_data =
 				&mcasp->dma_data[SNDRV_PCM_STREAM_PLAYBACK];
@@ -1225,9 +1226,15 @@ static int davinci_mcasp_probe(struct platform_device *pdev)
 	switch (mcasp->version) {
 	case MCASP_VERSION_1:
 	case MCASP_VERSION_2:
-	case MCASP_VERSION_3:
 		ret = davinci_soc_platform_register(&pdev->dev);
 		break;
+#if IS_BUILTIN(CONFIG_SND_EDMA_SOC) || \
+		(IS_MODULE(CONFIG_SND_DAVINCI_SOC_MCASP) && \
+		 IS_MODULE(CONFIG_SND_EDMA_SOC))
+	case MCASP_VERSION_3:
+		ret = edma_pcm_platform_register(&pdev->dev);
+		break;
+#endif
 	case MCASP_VERSION_4:
 		ret = omap_pcm_platform_register(&pdev->dev);
 		break;
