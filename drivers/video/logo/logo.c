aa9f020bbe7e25620b05a609ab52d9ee2bd7a6b9
Daniel Mack <daniel@zonque.org>
2018-06-15 22:43:30 +0200

    DOWNSTREAM: Add Raumfeld bootup logo

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -99,6 +99,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_RAUMFELD_CLUT224
+		/* Raumfeld boot logo */
+		logo = &logo_raumfeld_clut224;
+#endif
 	}
 	return logo;
 }
